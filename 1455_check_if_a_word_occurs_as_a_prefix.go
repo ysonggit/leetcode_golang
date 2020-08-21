c0d23b8ee2cdf9c87724d228cf1055139ea46349
Yang Song <ysong.sc@gmail.com>
2020-08-21 11:27:29 +0200

    100% time beat, 25% mem

--- 1455_check_if_a_word_occurs_as_a_prefix.go
+++ 1455_check_if_a_word_occurs_as_a_prefix.go
@@ -9,3 +9,22 @@ func isPrefixOfWord(sentence string, searchWord string) int {
 	}
 	return -1
 }
+
+func isPrefixOfWord_2(sentence string, searchWord string) int {
+	start := 0
+	n := len(searchWord)
+	word_idx := 0
+	for i := 0; i < len(sentence); i++ {
+		if ' ' == sentence[i] || i == len(sentence)-1 {
+			word_idx++
+			m := i - start + 1
+			//fmt.Println(sentence[start:start+m])
+			if m >= n && searchWord == sentence[start:start+n] {
+				return word_idx
+			} else {
+				start = i + 1
+			}
+		}
+	}
+	return -1
+}
