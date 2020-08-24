21a58d5c84529b360f3d5cf2aa85d351257e4973
Yang Song <ysong.sc@gmail.com>
2020-08-24 17:30:51 +0200

    100% time and 70% mem beat

--- 1108_defanging_an_ip_address.go
+++ 1108_defanging_an_ip_address.go
@@ -4,3 +4,9 @@ func defangIPaddr(address string) string {
 	re := regexp.MustCompile(`(\.)`)
 	return re.ReplaceAllString(address, "[.]")
 }
+
+import "strings"
+func defangIPaddr_2(address string) string {
+    tokens := strings.Split(address, ".")
+    return strings.Join(tokens[:], "[.]")
+}
