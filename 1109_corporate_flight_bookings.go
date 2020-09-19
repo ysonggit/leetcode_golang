3910d65cbb0a3b97cc2b2e07b2d1f81c7826043d
yang song <ysong.sc@gmail.com>
2020-09-19 23:46:51 +0200

    83% time beat, 6% mem

--- 1109_corporate_flight_bookings.go
+++ 1109_corporate_flight_bookings.go
@@ -1,9 +1,13 @@
 func corpFlightBookings(bookings [][]int, n int) []int {
 	res := make([]int, n)
 	for _, book := range bookings {
-		for i := book[0]; i <= book[1]; i++ {
-			res[i-1] += book[2]
+		res[book[0]-1] += book[2]
+		if book[1] < n {
+			res[book[1]] -= book[2]
 		}
+	}
+	for i := 1; i < n; i++ {
+		res[i] += res[i-1]
 	}
 	return res
 }
