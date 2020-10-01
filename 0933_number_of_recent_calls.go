1cdf88ce927487503b1003b906557b78e94e94d6
Yang Song <ysong.sc@gmail.com>
2020-10-01 16:49:27 +0200

    69% time 38% mem

---
+++ 0933_number_of_recent_calls.go
@@ -0,0 +1,19 @@
+type RecentCounter struct {
+	requests []int
+}
+
+func Constructor() RecentCounter {
+	return RecentCounter{[]int{}}
+}
+
+func (this *RecentCounter) Ping(t int) int {
+	this.requests = append(this.requests, t)
+	for len(this.requests) > 0 && this.requests[0] != t {
+		if t-this.requests[0] > 3000 {
+			this.requests = this.requests[1:]
+		} else {
+			break
+		}
+	}
+	return len(this.requests)
+}
